733cc1ed78ac5ec764e8f8d95a8bbbefa8c876a0
ethan-cooke <ethandcooke@gmail.com>
2024-01-17 05:47:10 -0400

    added content of head file

---
+++ src/common/head.php
@@ -0,0 +1,26 @@
+<head>
+  <meta charset="UTF-8" />
+  <?php
+    $path = dirname($_SERVER['PHP_SELF']);
+    $end = basename(dirname(__DIR__, 1));
+    $secure = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on';
+    $protocol = $secure ? 'https://' : 'http://';
+    $base = $protocol . $_SERVER['HTTP_HOST'] . strstr($path, $end, true) . $end . '/';
+  ?>
+  <base href="<?php echo $base ?>" />
+  <meta name="viewport" content="width=device-width initial-scale=1" />
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+          }
+        }
+      }
+    }
+  </script>
+  <link rel="stylesheet" href="css/my_css.css" />
+  <title>Boardarama</title>
+</head>
