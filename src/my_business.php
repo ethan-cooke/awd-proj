7d7bc8f5dc932b82ad021d7aea295805c0c91681
ethan-cooke <ethandcooke@gmail.com>
2024-01-19 11:32:58 -0400

    changes to nav bar and home page

--- src/my_business.php
+++ src/my_business.php
@@ -2,12 +2,13 @@
 <html lang="en">
 <?php include('common/head.php'); ?>
 <body>
-    <?php include('common/navbar.html'); ?> <!--Use prose for tailwind on content -->
-    <div class="flex justify-center bg-main-page bg-center w-full">
-        <div class="flex py-20">
-            hello, world!
+    <div class="flex flex-col justify-center w-full h-screen ">
+        <?php include('common/navbar.html'); ?> <!--Use prose for tailwind on content -->
+        <div class="flex justify-center bg-main-page bg-center w-full h-full items-center">
+            <div class="flex py-20">
+                hello, world!
+            </div>
         </div>
-
     </div>
     <div class="flex flex-col justify-center w-full max-w-6xl p-4 text-twhite">
         
