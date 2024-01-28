b446fe3b87b116c58a8e7f099fa82aab9fffef09
ethan-cooke <ethandcooke@gmail.com>
2024-01-28 19:57:40 -0400

    fixxed issues with site map and locations

--- src/pages/locations.php
+++ src/pages/locations.php
@@ -9,17 +9,17 @@
         <div class="prose max-w-none">
           <h1>Our Locations</h1>
         </div>
-        <div class="flex flex-col md:flex-row gap-2 bg-tblue1 md:rounded-full rounded-md oveflow-hidden">
+        <div class="flex flex-col md:flex-row gap-4 bg-tblue1 md:rounded-full rounded-md overflow-hidden">
           <img class="location-image" src="images/location1.png" alt="location 1">
-          <div class="p-2 md:pt-10 flex flex-col pl-4 prose max-w-none">
+          <div class="p-2 justify-center flex flex-col prose max-w-none">
             <h2>Dartmouth</h2>
             <span>341 Portland Hills Drive, Dartmouth NS, B2W 6R5</span>
             <span>Phone Number: 902-123-4567</span>
           </div>
         </div>
-        <div class="flex flex-col md:flex-row gap-2 bg-tblue1 md:rounded-full rounded-md oveflow-hidden">
+        <div class="flex flex-col md:flex-row gap-4 bg-tblue1 md:rounded-full rounded-md overflow-hidden">
           <img class="location-image" src="images/location2.png" alt="location 1">
-          <div class="p-2 md:pt-10 flex flex-col pl-4 prose max-w-none">
+          <div class="p-2 justify-center flex flex-col prose max-w-none">
             <h2>Halifax</h2>
             <span>6253 Allan St, Halifax NS, B3L 1G8</span>
             <span>Phone Number: 902-765-4321</span>
