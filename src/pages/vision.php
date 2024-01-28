6910ff98c8b4699dabc4b8dbf69fccf48606662a
Ethan Cooke <ethandcooke@gmail.com>
2024-01-27 23:05:16 -0400

    Added Vision and mission page

--- src/pages/vision.php
+++ src/pages/vision.php
@@ -4,9 +4,31 @@
 <body>
   <div class="flex flex-col justify-center w-full h-screen">
     <?php include('../common/navbar.php'); ?>
-    <div class="flex flex-col prose max-w-none h-full text-tgray p-6">
-      <h1>Our Vision</h1>
-      <p></p>
+    <div class="flex justify-center h-full">
+      <div class="flex max-w-6xl justify-center">
+        <div class="flex flex-col prose max-w-none text-tgray p-6">
+          <h1>Our Vision</h1>
+          <div class="flex gap-10">
+            <p>At TimeWarp Consoles, our mission is to bridge the gap between nostalgia and modern gaming, creating a haven for enthusiasts seeking
+              a journey back in time through the world of retro gaming consoles. Founded with a passion for preserving the golden era of gaming, our
+              company aims to curate a diverse collection of vintage game consoles, ensuring that the magic of classics like Atari, Nintendo, and Sega remains
+              accessible to both seasoned collectors and new-generation gamers. We believe in the intrinsic value of these iconic consoles, fostering an appreciation
+              for the rich history and innovation that laid the foundation for the gaming industry today. TimeWarp Consoles is not merely a store but a cultural hub,
+              where customers can embark on a trip down memory lane, rediscover the joy of pixelated adventures, and share the timeless experience of gaming with future
+              generations. Our commitment lies in curating quality, nostalgia-inducing gaming systems, providing a unique platform that transcends time and connects gamers
+              across different eras.</p>
+            <div>
+              <div class="hidden md:flex flex-col break-inside-avoid bg-stone-300 rounded-md">
+                <img class="bg-white overflow-hidden aspect-square" src="images/virtual_boy.png" alt="Virtual Boy" width="2000">
+                <div class="p-4 pt-0">
+                  Virtual Boy - 1995
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
     </div>
     <?php include('../common/footer.html'); ?>
   </div>
--- src/readme.txt
+++ src/readme.txt
@@ -5,12 +5,10 @@ into the layout I decided on for the navbar.
 
 Submission 1 todo: 
 
-Are header and descriptive material adequate, relevant, and grammatical-and-spelling-error-free? 
 Are there sufficient appropriate links in the dropdowns under the menu bar items? 
 Do all inactive links have the "inactive popup" and go to a "sorry page"? 
 Do active links go to pages with appropriate content and the same header and footer? 
-Is there an appropriate "e-store Options" page? [Think of this a "future proofing" prep for e-store implementation.] 
-Is Site Map OK, including all of its links? 
+Is there an appropriate "e-store Options" page? [Think of this a "future proofing" prep for e-store implementation.]
 Are business specific links and pages OK? 
 Do all pages exhibit the same "look and feel", and behave "responsively"? 
 Do all pages validate against HTML5 and CSS3? 
