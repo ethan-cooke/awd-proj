adafbe0d8a92ec10351f171d0c93b84747346c11
Ethan Cooke <ethandcooke@gmail.com>
2024-04-28 01:45:10 -0300

    updated db path in db_connect

--- src/scripts/db_connect.php
+++ src/scripts/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 class TimewarpDb extends SQLite3 {
     function __construct() {
-        $this->open($_SERVER['CONTEXT_DOCUMENT_ROOT']."../.sqlite/timewarp.db");
+        $this->open($_SERVER['CONTEXT_DOCUMENT_ROOT']."/../.sqlite/timewarp.db");
     }
 
     function fieldAlreadyExists($tableName, $fieldName, $fieldValue) {
